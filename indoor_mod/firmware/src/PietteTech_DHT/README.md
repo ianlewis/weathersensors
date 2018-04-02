5fdad7eef9c37ee7fe976c592d40f7a74f96277b
Ian Lewis <ianmlewis@gmail.com>
2018-04-02 21:15:59 +0900

    Add MQTT support

--- indoor_mod/firmware/src/PietteTech_DHT/README.md
+++
@@ -1,3 +0,0 @@
-Third party library for reading data from DHT22 sensors.
-
-https://github.com/piettetech/PietteTech_DHT
--- indoor_mod/firmware/src/application.cpp
+++ indoor_mod/firmware/src/application.cpp
@@ -6,12 +6,53 @@ a DHT22 temperature/humidity sensor
 /* Includes ------------------------------------------------------------------*/  
 
 #include "application.h"
+#include "config.h"
 #include "version.h"
 
 // PietteTech DHT22 library.
 // https://github.com/piettetech/PietteTech_DHT
-#include "PietteTech_DHT/PietteTech_DHT.h"
+#include "third_party/PietteTech_DHT/firmware/PietteTech_DHT.h"
 
+// MQTT TLS library
+// https://github.com/hirotakaster/MQTT-TLS
+#include "third_party/MQTT-TLS/src/MQTT-TLS.h"
+
+#define LETS_ENCRYPT_CA_PEM                                             \
+"-----BEGIN CERTIFICATE----- \r\n"                                      \
+"MIIFjTCCA3WgAwIBAgIRANOxciY0IzLc9AUoUSrsnGowDQYJKoZIhvcNAQELBQAw\r\n"  \
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\r\n"  \
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTYxMDA2MTU0MzU1\r\n"  \
+"WhcNMjExMDA2MTU0MzU1WjBKMQswCQYDVQQGEwJVUzEWMBQGA1UEChMNTGV0J3Mg\r\n"  \
+"RW5jcnlwdDEjMCEGA1UEAxMaTGV0J3MgRW5jcnlwdCBBdXRob3JpdHkgWDMwggEi\r\n"  \
+"MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCc0wzwWuUuR7dyXTeDs2hjMOrX\r\n"  \
+"NSYZJeG9vjXxcJIvt7hLQQWrqZ41CFjssSrEaIcLo+N15Obzp2JxunmBYB/XkZqf\r\n"  \
+"89B4Z3HIaQ6Vkc/+5pnpYDxIzH7KTXcSJJ1HG1rrueweNwAcnKx7pwXqzkrrvUHl\r\n"  \
+"Npi5y/1tPJZo3yMqQpAMhnRnyH+lmrhSYRQTP2XpgofL2/oOVvaGifOFP5eGr7Dc\r\n"  \
+"Gu9rDZUWfcQroGWymQQ2dYBrrErzG5BJeC+ilk8qICUpBMZ0wNAxzY8xOJUWuqgz\r\n"  \
+"uEPxsR/DMH+ieTETPS02+OP88jNquTkxxa/EjQ0dZBYzqvqEKbbUC8DYfcOTAgMB\r\n"  \
+"AAGjggFnMIIBYzAOBgNVHQ8BAf8EBAMCAYYwEgYDVR0TAQH/BAgwBgEB/wIBADBU\r\n"  \
+"BgNVHSAETTBLMAgGBmeBDAECATA/BgsrBgEEAYLfEwEBATAwMC4GCCsGAQUFBwIB\r\n"  \
+"FiJodHRwOi8vY3BzLnJvb3QteDEubGV0c2VuY3J5cHQub3JnMB0GA1UdDgQWBBSo\r\n"  \
+"SmpjBH3duubRObemRWXv86jsoTAzBgNVHR8ELDAqMCigJqAkhiJodHRwOi8vY3Js\r\n"  \
+"LnJvb3QteDEubGV0c2VuY3J5cHQub3JnMHIGCCsGAQUFBwEBBGYwZDAwBggrBgEF\r\n"  \
+"BQcwAYYkaHR0cDovL29jc3Aucm9vdC14MS5sZXRzZW5jcnlwdC5vcmcvMDAGCCsG\r\n"  \
+"AQUFBzAChiRodHRwOi8vY2VydC5yb290LXgxLmxldHNlbmNyeXB0Lm9yZy8wHwYD\r\n"  \
+"VR0jBBgwFoAUebRZ5nu25eQBc4AIiMgaWPbpm24wDQYJKoZIhvcNAQELBQADggIB\r\n"  \
+"ABnPdSA0LTqmRf/Q1eaM2jLonG4bQdEnqOJQ8nCqxOeTRrToEKtwT++36gTSlBGx\r\n"  \
+"A/5dut82jJQ2jxN8RI8L9QFXrWi4xXnA2EqA10yjHiR6H9cj6MFiOnb5In1eWsRM\r\n"  \
+"UM2v3e9tNsCAgBukPHAg1lQh07rvFKm/Bz9BCjaxorALINUfZ9DD64j2igLIxle2\r\n"  \
+"DPxW8dI/F2loHMjXZjqG8RkqZUdoxtID5+90FgsGIfkMpqgRS05f4zPbCEHqCXl1\r\n"  \
+"eO5HyELTgcVlLXXQDgAWnRzut1hFJeczY1tjQQno6f6s+nMydLN26WuU4s3UYvOu\r\n"  \
+"OsUxRlJu7TSRHqDC3lSE5XggVkzdaPkuKGQbGpny+01/47hfXXNB7HntWNZ6N2Vw\r\n"  \
+"p7G6OfY+YQrZwIaQmhrIqJZuigsrbe3W+gdn5ykE9+Ky0VgVUsfxo52mwFYs1JKY\r\n"  \
+"2PGDuWx8M6DlS6qQkvHaRUo0FMd8TsSlbF0/v965qGFKhSDeQoMpYnwcmQilRh/0\r\n"  \
+"ayLThlHLN81gSkJjVrPI0Y8xCVPB4twb1PFUd2fPM3sA1tJ83sZ5v8vgFv2yofKR\r\n"  \
+"PB0t6JzUA81mSqM3kxl5e+IZwhYAyO0OTg3/fs8HqGTNKd9BqoUwSRBzp06JMg5b\r\n"  \
+"rUCGwbCUDI0mxadJ3Bz4WxR6fyNpBK2yAinWEsikxqEt\r\n"  \
+"-----END CERTIFICATE----- "
+const char letsencryptCaPem[] = LETS_ENCRYPT_CA_PEM;
+
+const int LOOP_INTERVAL_SEC = 60;
 const int READ_LED = D0;
 const int DHTPIN = D1;
 
@@ -29,6 +70,29 @@ void dht_wrapper() {
 #define ONE_DAY_MILLIS (24 * 60 * 60 * 1000)
 unsigned long lastSync = millis();
 
+double humidity = 0;
+double temp = 0;
+String deviceName = String("");
+String location = String(CONFIG_LOCATION);
+String localIP;
+String deviceType = String("indoor_mod");
+String version = String(gitversion);
+
+struct State {
+    bool wifiReady;
+    bool particleConnected;
+    bool mqttConnected;
+    State() : wifiReady(false), particleConnected(false), mqttConnected(false) {}
+    bool ready() {
+        return wifiReady && particleConnected && mqttConnected; 
+    }
+};
+
+State prevState, currentState;
+
+// Initialized the client and set keepalive to 2 * loop interval
+MQTT client(CONFIG_MQTT_HOST, CONFIG_MQTT_PORT, LOOP_INTERVAL_SEC*2, NULL);
+
 /*
  * Sync the device time with the spark server.
  */
@@ -41,18 +105,15 @@ void syncTime() {
 }
 
 void log(String msg) {
-    if (Serial.available()) {
+    if (Serial.isConnected()) {
         Serial.println(String("[") + String(Time.now()) + String("] ") + msg);
     }
 }
 
-
-double humidity = 0;
-double temp = 0;
-String location = String("unknown");
-String localIP;
-String deviceType = String("indoor_mod");
-String version = String(gitversion);
+void deviceNameHandler(const char *topic, const char *data) {
+    deviceName = String(data);
+    log("Got device name: "+deviceName);
+}
 
 void setup() {
     // start listening for clients
@@ -67,27 +128,71 @@ void setup() {
     Particle.variable("localIP", localIP);
     Particle.variable("version", version);
 
-    // Delay 15 seconds so we can connect for debugging
-    delay(15000);
+    // Get the device name
+    Particle.subscribe("particle/device/name", deviceNameHandler);
+    Particle.publish("particle/device/name");
+
+    // Enable tls. set Root CA pem file.
+    client.enableTls(letsencryptCaPem, sizeof(letsencryptCaPem));
+
+    // Delay 10 seconds so we can connect for debugging
+    waitFor(Serial.isConnected, 10000);
+
+    log("setup finished");
 }
 
 // The main loop that gets run forever.
 void loop() {
+    int extra = 0;
+
     // Sync with the Spark server if necessary.
     syncTime();
 
-    int extra = 0;
+    // Wait for the deviceName before looping
+    if (deviceName == "") {
+        log("Waiting for device name...");
+        delay(100);
+        return;
+    }
 
-    localIP = String(WiFi.localIP());
+    // Run the mqtt client loop
+    client.loop();
 
-    // If not connected to the WiFi or Particle API then blink.
-    if (!WiFi.ready() || !Particle.connected()) {
+    currentState = State();
+    currentState.wifiReady = WiFi.ready();
+    currentState.particleConnected = Particle.connected();
+    currentState.mqttConnected = client.isConnected();
+
+    if (!currentState.mqttConnected) {
+        // Attempt to re-connect to the MQTT server
+        log("Connecting to mqtt server");
+        if (client.connect(deviceName, CONFIG_MQTT_USERNAME, CONFIG_MQTT_PASSWORD)) {
+            log("Connected to mqtt server");
+        } else {
+            log("Failed to connect to mqtt server");
+        }
+        currentState.mqttConnected = client.isConnected();
+    }
+
+    // Log changes
+    if (!prevState.wifiReady && currentState.wifiReady) {
+        log("Connected to WiFi");
+    }
+    if (!prevState.particleConnected && currentState.particleConnected) {
+        log("Connected to Particle");
+    }
+    prevState = currentState;
+
+    // If not ready then blink
+    if (!currentState.ready()) {
         digitalWrite(READ_LED, HIGH);
         delay(1000);
         digitalWrite(READ_LED, LOW);
         delay(1000);
         return;
     }
+
+    localIP = String(WiFi.localIP());
 
     // Turn on the READ LED.
     digitalWrite(READ_LED, HIGH);
@@ -115,16 +220,18 @@ void loop() {
         temp = fTemp;
         humidity = fHumidity;
 
-        String data = String("timestamp:") + String(Time.now()) + String("\ttemp:") + String(temp) + String("\thumidity:") + String(humidity);
-        Particle.publish("weatherdata", data);
+        String data = String("{\"location\":\""+location+"\",\"timestamp\":" + String(Time.now()) + ",\"temperature\":" + String(temp) +",\"humidity\":" + String(humidity) + "}");
+        client.publish("home/" + location + "/climate", data);
+
         log(data);
     }
 
     // Delay so that the READ LED stays on
     // for a little longer.
     delay(100);
+    extra += 100;
     digitalWrite(READ_LED, LOW);
 
-    // Send data every 1 min.
-    delay(1000 * 60 - 100 - extra);
+    // Send data every interval
+    delay(1000 * LOOP_INTERVAL_SEC - extra);
 }
